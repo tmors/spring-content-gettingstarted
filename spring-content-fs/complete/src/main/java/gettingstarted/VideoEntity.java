218eac4e81103bd1520126c9ae6d7895b50554e4
pengyuxiang <398080994@163.com>
2018-12-21 23:32:06 +0800

    Init

---
+++ spring-content-fs/complete/src/main/java/gettingstarted/VideoEntity.java
@@ -0,0 +1,26 @@
+package gettingstarted;
+
+/**
+ * @author pengyuxiang
+ * @date 2018/12/21
+ */
+public class VideoEntity {
+    private String title;
+    private String videoUrl;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+}
